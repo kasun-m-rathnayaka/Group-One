388a0690c258bbd16ab46e6eafaed898952e8f22
Kasun Rathnayaka <k.rathnayaka365@gmail.com>
2024-12-11 14:30:09 +0530

    working funding model

--- group_01_firm/models.py
+++ group_01_firm/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Firm(models.Model):
+    name = models.CharField(max_length=100)
+    cpm = models.CharField(max_length=100)
+    investment = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
--- group_01_firm/views.py
+++ group_01_firm/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Firm
 
 # Create your views here.
+def funding(request):
+    if request.method == 'POST':
+        print(request.POST)
+        name = request.POST['name']
+        cpm = request.POST['cpm']
+        investment = request.POST['investment']
+        Firm.objects.create(name=name, cpm=cpm, investment=investment)
+    context = {
+        'fundings': Firm.objects.all()
+    }
+    return render(request, 'funding.html', context)
+
+
+def home(request):
+    return render(request, 'home.html')
+
+
+def contact(request):
+    return render(request, 'contact.html')
+
+
+def about(request):
+    return render(request, 'about.html')
