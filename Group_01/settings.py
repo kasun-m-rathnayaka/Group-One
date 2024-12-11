893673fdaa07fc9b38fb30efe3336fba4d4d9df9
Kasun Rathnayaka <k.rathnayaka365@gmail.com>
2024-12-11 13:47:41 +0530

    app initialization

--- Group_01/settings.py
+++ Group_01/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "group_01_firm.apps.Group01FirmConfig"
 ]
 
 MIDDLEWARE = [
